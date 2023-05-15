a31e1fd359dcd6940251306a54ce1f7aaeeddf83
jac <solomon.noname@gmail.com>
2023-05-16 11:45:24 +1200

    added thymeleaf

--- src/main/java/com/example/bmicalculator2/controller/BmiCalculatorController.java
+++ src/main/java/com/example/bmicalculator2/controller/BmiCalculatorController.java
@@ -1,20 +1,30 @@
 package com.example.bmicalculator2.controller;
 
 
+
 import com.example.bmicalculator2.service.BmiCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class BmiCalculatorController {
 
     @Autowired
     private BmiCalculatorService bmiCalculatorService;
 
-    @GetMapping("/calculateBmi")
-    public double calculateBmi(@RequestParam double weight, @RequestParam double height) {
-        return bmiCalculatorService.calculate(weight, height);
+    @GetMapping("/bmiForm")
+    public String bmiForm() {
+        return "bmiForm";
+    }
+
+    @PostMapping("/calculateBmi")
+    public String calculateBmi(@RequestParam double weight, @RequestParam double height, Model model) {
+        double bmi = bmiCalculatorService.calculate(weight, height);
+        model.addAttribute("bmi", bmi);
+        return "bmiResult";
     }
 }

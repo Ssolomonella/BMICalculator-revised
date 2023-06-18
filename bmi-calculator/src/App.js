d8801919edf4a855c16ef34e939d20b047d8e829
jac <solomon.noname@gmail.com>
2023-06-18 13:12:26 +1200

    added React

---
+++ bmi-calculator/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import BmiForm from './BmiForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BmiForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
